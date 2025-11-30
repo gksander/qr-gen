8c0c03aed1edbf8a8e9597741ef1836ef8d6ed07
Grant Sander <gksander93@gmail.com>
2025-11-29 21:39:58 -0600

    KV caching

--- src/routes/c.$id.ts
+++ src/routes/c.$id.ts
@@ -1,12 +1,22 @@
 import { db } from '@/db'
 import { qrCodesTable } from '@/db/schema'
 import { createFileRoute } from '@tanstack/react-router'
+import { env } from 'cloudflare:workers'
 import { eq } from 'drizzle-orm'
 
 export const Route = createFileRoute('/c/$id')({
   server: {
     handlers: {
+      /**
+       * TODO: log to analytics. Proably only log to analytics if redirect hit
+       */
       GET: async ({ params }) => {
+        // Check the cache
+        const storedUrl = await env.KV.get(params.id)
+        if (storedUrl) {
+          return Response.redirect(storedUrl, 307)
+        }
+
         const code = await db.query.qrCodesTable.findFirst({
           where: eq(qrCodesTable.id, params.id),
         })
@@ -24,6 +34,9 @@ export const Route = createFileRoute('/c/$id')({
             status: 400,
           })
         }
+
+        // Stash in cache
+        await env.KV.put(params.id, code.data)
 
         return Response.redirect(code.data, 307)
       },
